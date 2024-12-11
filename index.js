19e850f707e55c4cb4c6bb27a97f2289bba8755d
Abdullahi Abdikadir <aabdikadir849@gmail.com>
2024-12-11 10:00:10 +0300

    completed array reduce

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// reducer.js
+
+// Sample array of battery counts
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Reducer function to calculate the total batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+module.exports = { totalBatteries };
+
+
